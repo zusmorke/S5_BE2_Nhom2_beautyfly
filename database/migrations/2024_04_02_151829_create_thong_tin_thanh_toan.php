8e80eb0428c340077433f02f1594359ddd2e3285
ThoGtb <ngothithunc2004@gmail.com>
2024-04-02 22:37:38 +0700

    Ky

---
+++ database/migrations/2024_04_02_151829_create_thong_tin_thanh_toan.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('thong_tin_thanh_toan', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('donhang_id')->unsigned();
+            $table->string('ten', 50);
+            $table->string('tinh', 255);
+            $table->string('diachi', 255);
+            $table->string('email', 100)->unique();
+            $table->string('sdt', 255);
+            $table->string('ghichudonhang', 255)->nullable();
+            
+            
+            // Khóa ngoại
+            $table->foreign('user_id')->references('user_id')->on('users');
+            $table->foreign('donhang_id')->references('donhang_id')->on('don_hang');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('thong_tin_thanh_toan');
+    }
+};
