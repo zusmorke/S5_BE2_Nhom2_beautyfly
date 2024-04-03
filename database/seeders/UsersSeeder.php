369fdd6d0c130fecb97861bf05c275c9da58882c
zusmorke <Dinhlove2k4@gmail.com>
2024-04-03 09:42:55 +0700

    mha

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 
-class UsersTableSeeder extends Seeder
+class UsersSeeder extends Seeder
 {
     /**
      * Run the database seeds.
